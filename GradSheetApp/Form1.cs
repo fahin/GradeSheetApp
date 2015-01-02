19b1ed11522a60ea303b40aba45e91db6f2b69a5
fahins@yahoo.com <fahins@yahoo.com>
2015-01-03 00:14:23 +0600

    update

---
+++ GradSheetApp/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GradSheetApp
+{
+    public partial class GradeSheetForm : Form
+    {
+        public GradeSheetForm()
+        {
+            InitializeComponent();
+        }
+
+        private void showButton_Click(object sender, EventArgs e)
+        {
+            GradeSheet grade = new GradeSheet();
+            grade.Physics = Convert.ToDouble(physicsTextBox.Text);
+            grade.Chemistry = Convert.ToDouble(chemistryTextBox.Text);
+            grade.Math = Convert.ToDouble(mathTextBox.Text);
+
+            grade.averageCalculation();
+            averageTextBox.Text = Convert.ToString(grade.Average);
+           grade.gradeCalculation();
+           gradeTextBox.Text = grade.Grade;
+
+
+        }
+    }
+}
---
+++ GradSheetApp/GradeSheet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradSheetApp
+{
+    class GradeSheet
+    {
+        private double physics;
+
+        public double Physics
+        {
+            get { return physics; }
+            set { physics = value; }
+        }
+        private double chemistry;
+
+        public double Chemistry
+        {
+            get { return chemistry; }
+            set { chemistry = value; }
+        }
+        private double math;
+
+        public double Math
+        {
+            get { return math; }
+            set { math = value; }
+        }
+        private double average;
+
+        public double Average
+        {
+            get { return average; }
+            set { average = value; }
+        }
+        private string grade;
+
+        public string Grade
+        {
+            get { return grade; }
+            set { grade = value; }
+        }
+
+
+        public void averageCalculation()
+        {
+            average = (physics + chemistry + math) / 3;
+        
+        }
+        public void gradeCalculation()
+        {
+
+            if (average>=80)
+            {
+                grade = "A+";
+
+            }
+            if (average >= 70 && average < 80)
+            {
+                grade = "B+";
+            }
+            if (average >= 60 && average < 70)
+            {
+                grade = "C+";
+            }
+            if (average >= 50 && average < 60)
+            {
+                grade = "D+";
+            }
+            if(average<50)
+            {
+                grade = "F";
+            }
+        }
+    }
+}
